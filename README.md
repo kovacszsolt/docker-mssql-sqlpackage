62300007440018d73f0e90d9f2882ebe9913cd37
Zsolt Kovács <kovacs.zsolt@nexius.hu>
2021-08-20 23:15:04 +0200

    add export / import parameter

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ Docker Image SQLPackage command
 - Database = Database name  
 - Username = Database username  
 - Password = Database password  
+
+## Output
+/output/$Database
+
+## Example
+  
+### Import  
+docker run -v .\database_files:/output -e Action=import -e Port=1433 -e Hostname=host.docker.internal -e Database=databasename -e Username=sa -e Password=sa123 kovacszsolt/mssql-sqlpackage  
+  
+### Export  
+docker run -v .\database_files:/output -e Action=export -e Port=1433 -e Hostname=database.host -e Database=databasename -e Username=sa -e Password=sa123  kovacszsolt/mssql-sqlpackage  
+  
+  
--- command.sh
+++ command.sh
@@ -1,1 +1,20 @@
-/utils/sqlpackage/sqlpackage /Action:$Action /ssn:tcp:$Hostname,$Port /sdn:$Database /su:$Username /sp:$Password /tf:/output/$Database.bacpac
+VAR_EXPORT="EXPORT"
+VAR_PARAM_ACTION=${Action^^}
+VAR_IMPORT="IMPORT"
+
+if [ "$VAR_PARAM_ACTION" = "$VAR_EXPORT" ]; then
+    echo "EXPORT"
+    /utils/sqlpackage/sqlpackage /Action:Export /ssn:tcp:$Hostname,$Port /sdn:$Database /su:$Username /sp:$Password /tf:/output/$Database.bacpac
+    exit 0
+fi
+
+if [ "$VAR_PARAM_ACTION" = "$VAR_IMPORT" ]; then
+    echo "IMPORT"
+    /utils/sqlpackage/sqlpackage /Action:Import /tsn:tcp:$Hostname,$Port /tdn:$Database /tu:$Username /tp:$Password /sf:/output/$Database.bacpac
+    exit 0
+fi
+
+if [[ "$VAR_PARAM_ACTION" != "$VAR_IMPORT" && "$VAR_PARAM_ACTION" != "$VAR_EXPORT" ]]; then
+    echo "unkown Action Parameter"
+    exit 1
+fi
